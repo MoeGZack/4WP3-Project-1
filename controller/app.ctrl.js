0fd54c5edddf638a9baffdfa3dd4c857bda5655f
Mohammad Ameen <mohammad.ameen@powerhouse.ca>
2026-02-26 20:47:52 -0500

    Adding node_modules, Also. Got a issue with starting node server. My app model path appears to be wrong. I will need to adjust this to fix the server to start

---
+++ controller/app.ctrl.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mustacheExpress=require('mustache-express');
+const port=3000;
+const hostname="localhost";
+
+const Model=require("model/.app.model.js");
+app.engine('mustache', mustacheExpress());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'mustache');
+
+
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true })); 
+
+
+async function main(){
+    await Model.makeConnection();
+
+    app.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+}
+
+main();
