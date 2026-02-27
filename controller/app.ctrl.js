ad057afcbf1b6389c53e45bec37e1888ef5793eb
Mohammad Ameen <mohammad.ameen@powerhouse.ca>
2026-02-26 21:10:18 -0500

    Added the links to mustache and controller. Issue was I was looking for views in the controller folder. Looked through console logs to debug

--- controller/app.ctrl.js
+++ controller/app.ctrl.js
@@ -5,13 +5,17 @@ const port=3000;
 const hostname="localhost";
 
 const Model=require('../model/app.model.js');
-app.engine('mustache', mustacheExpress());
-app.set('views', __dirname + '/views');
-app.set('view engine', 'mustache');
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/../views");
+app.get("/app", async (req, res) => {
+    res.render("main");
+});
 
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/../public'));
 app.use(express.urlencoded({ extended: true })); 
+
 
 
 async function main(){
