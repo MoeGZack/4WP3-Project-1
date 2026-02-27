b41a5bdb1feac1340fd225eb9858028866b13b68
Mohammad Ameen <mohammad.ameen@powerhouse.ca>
2026-02-27 04:58:56 -0500

    Struggling to figure out the mustache->Db handshake. I mayjust utilize seperate text boxes to prevent these issues with using a generic object

--- controller/app.ctrl.js
+++ controller/app.ctrl.js
@@ -17,6 +17,32 @@ app.use(express.static(__dirname+'/../public'));
 app.use(express.urlencoded({ extended: true })); 
 
 
+app.post("/searchmember", async (req, res) => {
+        
+    const SearchTerm = req.body.searchReq;
+
+    const member = await Model.getmember(SearchTerm);
+    
+    if (!member) {
+        return res.render ("main",{
+            message: "No member found with the provided information.",
+            member:{}
+        });
+    } else {
+        console.log(member);
+    }
+    res.render("main", {
+        member:member,
+        isBasic: member.plan === "Basic",
+        isIntermediate: member.plan === "Intermediate",
+        isUltra: member.plan === "Ultra",
+        isActive:member.status === "Active",
+        isHold: member.status === "Hold",
+        isCancelled: member.status === "Cancelled"
+    });
+
+
+    });
 
 async function main(){
     await Model.dbconn();
--- model/app.model.js
+++ model/app.model.js
@@ -5,17 +5,32 @@ let db;
 
 async function dbconn() {
     db = await sqlite.open({
+        
         filename: "gym.db",
         driver: sqlite3.Database    
     });
+    await db.exec("PRAGMA foreign_keys = ON");
     console.log("Connected to database");
+    
 }
 
 
+async function getmember(SearchTerm) {
 
+    const member = await db.get(
+        `SELECT * FROM members 
+         WHERE (first_name like '${SearchTerm}%' or last_name like '${SearchTerm}%' or phone like '${SearchTerm}%')`, 
+   
+    );
+    console.log(member);
+    return member;
+    
+}
 
 module.exports = {
     
     dbconn,
+    getmember
+
     
 }
